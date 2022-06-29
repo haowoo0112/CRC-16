99e910e7b20996fee63cbab9143a6af2ae6d6174
haowoo <haowoo0112@gmail.com>
2022-06-29 14:40:52 +0800

    fix:error

--- CRC-16.c
+++ CRC-16.c
@@ -2,7 +2,7 @@
 #include "CRC-16.h"
 
 
-void CRC_16_cal(struct data *frame){
+void CRC_16_cal(struct data *frame, int *CRC_result){
 	int lowest_bit;
 	int i=0, j=0;
 	for(j = 0 ; j < 6 ; j++){
@@ -26,15 +26,15 @@ void CRC_16_cal(struct data *frame){
 
 int main(){
 	struct data rail;
-//	printf("請輸入六個數字，中間使用空白區隔）：");
-//    scanf("%x %x %x %x %x %x", &rail.slave_address, &rail.function, &rail.register_address[0], &rail.register_address[1], &rail.data[0], &rail.data[1]);
+	printf("請輸入六個數字，中間使用空白區隔）：");
+    scanf("%x %x %x %x %x %x", &rail.slave_address, &rail.function, &rail.register_address[0], &rail.register_address[1], &rail.data[0], &rail.data[1]);
 	
-	rail.slave_address = 0x01;
-	rail.function = 0x06;
-	rail.register_address[0] = 0x02;
-	rail.register_address[1] = 0xCB;
-	rail.data[0] = 0x00;
-	rail.data[1] = 0x00;
+//	rail.slave_address = 0x01;
+//	rail.function = 0x06;
+//	rail.register_address[0] = 0x02;
+//	rail.register_address[1] = 0xCB;
+//	rail.data[0] = 0x00;
+//	rail.data[1] = 0x00;
 	rail.CRC_result = 0xffff;
 	
 	rail.whole[0] = rail.slave_address;
@@ -46,6 +46,6 @@ int main(){
 	rail.whole[6] = rail.CRC_result;
 
 	
-	CRC_16_cal(&rail);
+	CRC_16_cal(&rail, &rail.CRC_result);
 	
 } 
--- CRC-16.h
+++ CRC-16.h
@@ -8,7 +8,7 @@ struct data{
 };
 
 
-void CRC_16_cal(struct data*, int*, int*, int* );
+void CRC_16_cal(struct data*, int*);
 
 
 
